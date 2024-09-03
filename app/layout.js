43a0a4390a1864e031e6e0417df2ccf69ee3799e
Grosu Antonio <codevs@Grosus-MacBook-Air.local>
2024-09-03 17:14:00 +0300

    added google verify

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta
+          name="google-site-verification"
+          content="wSrOt8olR5ucIcj1sF3_KkSNH9uspGEv4Fped0pnCjU"
+        />
         <link rel="icon" href="/favicon.png" sizes="any" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </head>
